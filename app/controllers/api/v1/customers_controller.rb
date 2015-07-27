e647d4351f2ed0e46eaf2c4fa18fc8ee66a3518f
Morgan Miller <m.miller722@gmail.com>
2015-07-27 17:20:03 -0600

    add find and find_all to customers

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -2,10 +2,32 @@ class Api::V1::CustomersController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Customer.all
+    if params[:customers]
+      respond_with params[:customers]
+    else
+      respond_with Customer.all
+    end
   end
 
   def show
     respond_with Customer.find_by(id: params[:id])
+  end
+
+  def find
+    render json: Customer.find_by(find_params)
+  end
+
+  def find_all
+    render json: Customer.where(find_params)
+  end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :id, :last_name)
+  end
+
+  def find_params
+    params.permit(:id, :first_name, :last_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/customers/find', to: 'customers#find'
+      get 'customers/find_all', to: 'customers#find_all'
       resources :customers, except: [:edit, :new]
     end
   end
