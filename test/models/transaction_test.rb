154c05b5aa85251335c459cc3962e218c6072692
Morgan Miller <m.miller722@gmail.com>
2015-07-28 09:04:46 -0600

    add transaction model tests

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it belongs to an invoice" do
+    t = Transaction.first
+    i = Invoice.first
+
+    i.transactions << t
+    assert_equal i, t.invoice
+  end
+
+  test "it has many invoice items" do
+    ii = InvoiceItem.first
+    i = Invoice.first
+    t = Transaction.first
+
+    i.invoice_items << ii
+    i.transactions << t
+    assert_equal 1, t.invoice_items.count
+  end
+
+  test "it scopes for successful transactions" do
+    t1 = Transaction.create(result: "success")
+    t2 = Transaction.create(result: "failed")
+
+    assert Transaction.all.count > 1
+    assert_equal 1, Transaction.successful.count
+  end
 end
