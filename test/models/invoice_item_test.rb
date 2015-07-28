a58a13e5f28b2d4d0fb5c047cf25360a8438add8
Morgan Miller <m.miller722@gmail.com>
2015-07-28 09:04:03 -0600

    add invoice item model tests

--- test/models/invoice_item_test.rb
+++ test/models/invoice_item_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class InvoiceItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it belongs to an item" do
+    i = Item.first
+    ii = InvoiceItem.first
+
+    i.invoice_items << ii
+    assert_equal i, ii.item
+  end
+
+  test "it belongs to an invoice" do
+    i = Invoice.first
+    ii = InvoiceItem.first
+
+    i.invoice_items << ii
+    assert_equal i, ii.invoice
+  end
+
+  test "it knows its total cost" do
+    i = InvoiceItem.create(unit_price: 300, quantity: 2)
+
+    assert_equal 600, i.total_cost
+  end
 end
