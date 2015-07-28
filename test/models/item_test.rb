c33c021fe4e28e8ced3dd0060b48ca4940e83b5e
Morgan Miller <m.miller722@gmail.com>
2015-07-28 09:04:23 -0600

    add item model test

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it belongs to a merchant" do
+    m = Merchant.first
+    i = Item.first
+
+    m.items << i
+    assert_equal m, i.merchant
+  end
+
+  test "it has many invoice items" do
+    ii = InvoiceItem.first
+    i = Item.first
+
+    i.invoice_items << ii
+    assert_equal 1, i.invoice_items.count
+  end
 end
