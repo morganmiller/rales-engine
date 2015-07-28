696640325a7a7efc1ca82ecfa6a593937ccfa9a2
Morgan Miller <m.miller722@gmail.com>
2015-07-28 09:04:34 -0600

    add merchant model test

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class MerchantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it has many invoices" do
+    m = Merchant.first
+    i = Invoice.first
+
+    m.invoices << i
+    assert_equal 1, m.invoices.count
+  end
+
+  test "it has many items" do
+    m = Merchant.first
+    i = Item.first
+
+    m.items << i
+    assert_equal 1, m.items.count
+  end
+
+  test "it has many transactions" do
+    m = Merchant.first
+    i = Invoice.first
+    t = Transaction.first
+
+    i.transactions << t
+    m.invoices << i
+    assert_equal 1, m.transactions.count
+  end
 end
