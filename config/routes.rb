37253ff63141700bc75105e78e12388ed021323d
Morgan Miller <m.miller722@gmail.com>
2015-07-27 18:04:47 -0600

    add all routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,23 @@ Rails.application.routes.draw do
       get '/merchants/find_all', to: 'merchants#find_all'
       get '/merchants/random', to: 'merchants#random'
       resources :merchants, except: [:edit, :new]
+
+      get '/items/find', to: 'items#find'
+      get '/items/find_all', to: 'items#find_all'
+      get '/items/random', to: 'items#random'
+      resources :items, except: [:edit, :new]
+
+      get '/invoice_items/find', to: 'invoice_items#find'
+      get '/invoice_items/find_all', to: 'invoice_items#find_all'
+      get '/invoice_items/random', to: 'invoice_items#random'
+      resources :invoice_items, except: [:edit, :new]
+
+      get '/invoices/find', to: 'invoices#find'
+      get '/invoices/find_all', to: 'invoices#find_all'
+      get '/invoices/random', to: 'invoices#random'
+      resources :invoices, except: [:edit, :new]
+
+      resources :transactions, except: [:edit, :new]
     end
   end
 end
