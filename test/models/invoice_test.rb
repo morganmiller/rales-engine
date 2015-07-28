09f7daa4b4b21a3db64a8c846a8e7c786f13ede3
Morgan Miller <m.miller722@gmail.com>
2015-07-28 09:04:14 -0600

    add invoice model tests

--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
 class InvoiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it belongs to a customer" do
+    i = Invoice.first
+    c = Customer.first
+
+    c.invoices << i
+    assert_equal c, i.customer
+  end
+
+  test "it belongs to a merchant" do
+    i = Invoice.first
+    m = Merchant.first
+
+    m.invoices << i
+    assert_equal m, i.merchant
+  end
+
+  test "it has many transactions" do
+    i = Invoice.first
+    t = Transaction.first
+
+    i.transactions << t
+    assert_equal 1, i.transactions.count
+  end
+
+  test "it has many invoice items" do
+    i = Invoice.first
+    ii = InvoiceItem.first
+
+    i.invoice_items << ii
+    assert_equal 1, i.invoice_items.count
+  end
+
+  test "it has many items" do
+    invoice = Invoice.first
+    ii = InvoiceItem.first
+    item = Item.first
+
+    ii.item = item
+    invoice.invoice_items << ii
+    assert_equal 1, invoice.items.count
+  end
+
+  test "it scopes for associated successful transactions" do
+    t1 = Transaction.create(result: "success")
+    t2 = Transaction.create(result: "failed")
+
+    i1 = Invoice.first
+    i2 = Invoice.last
+    i1.transactions << t1
+    i2.transactions << t2
+
+    assert_equal 2, Invoice.all.count
+    assert_equal 1, Invoice.successful.count
+  end
 end
