1459fd9b9d4dcfeb4bfd3a62ca03fcf1e4514a08
Morgan Miller <m.miller722@gmail.com>
2015-07-28 09:03:51 -0600

    add customer model tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "it has many invoices" do
+    c = Customer.first
+    i = Invoice.first
+
+    c.invoices << i
+    assert_equal 1, c.invoices.count
+  end
+
+  test "it has many transactions" do
+    t = Transaction.first
+    i = Invoice.first
+    c= Customer.first
+
+    i.transactions << t
+    c.invoices << i
+    assert_equal 1, c.transactions.count
+  end
 end
