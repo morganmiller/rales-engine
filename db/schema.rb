043db02306edf386d0d7de1ed5e3d56fba47ffd2
Morgan Miller <m.miller722@gmail.com>
2015-07-29 13:45:17 -0600

    remove expiration date of cc from transactions

--- db/schema.rb
+++ db/schema.rb
@@ -58,10 +58,9 @@ ActiveRecord::Schema.define(version: 20150727200937) do
   create_table "transactions", force: :cascade do |t|
     t.integer  "invoice_id"
     t.string   "credit_card_number"
-    t.string   "credit_card_expiration_date"
     t.string   "result"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
