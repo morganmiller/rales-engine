c7e1b670b1bfdb31d83880f5276930cf4ffd4d32
Morgan Miller <m.miller722@gmail.com>
2015-07-28 18:23:25 -0600

    add favorite merchant to customer

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -21,13 +21,13 @@ class Api::V1::CustomersController < ApplicationController
     respond_with Customer.limit(1).order("RANDOM()")
   end
 
+  def favorite_merchant
+    customer = Customer.find_by(id: params[:id])
+    render json: customer.favorite_merchant
+  end
 private
 
   def find_params
     params.permit(:id, :first_name, :last_name)
   end
 end
-
-# def customer_params
-#   params.require(:customer).permit(:first_name, :id, :last_name)
-# end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,11 @@
 class Customer < ActiveRecord::Base
   has_many :invoices
   has_many :transactions, through: :invoices
+  has_many :merchants, through: :invoices
+
+  def favorite_merchant
+    merchants.max_by do |m|
+      m.invoices.successful.where(customer_id: self.id).count
+    end
+  end
 end
